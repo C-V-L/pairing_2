8957eb093e5626b61de3f25ba6445df535cba586
Conner Van Loan <cdvanloan@gmail.com>
2022-11-02 17:06:56 -0600

    Add places to visit

--- friends.rb
+++ friends.rb
@@ -6,3 +6,8 @@ puts friend_list
 puts friend_list[2]
 puts friend_list[-1]
 puts friend_list[0]
+
+places_to_visit = ["yosemite", "canyonlands", "arches", 'saguaro', 'rocky mountain']
+
+puts places_to_visit.pop
+
